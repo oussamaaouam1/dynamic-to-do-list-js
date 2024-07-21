b7e3828e7fcaaf297ebce4fc44374fc300a159f2
oussamaaouam1 <oussama.aouam@gmail.com>
2024-07-21 20:16:54 +0100

    commit to check the updates

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded' , () =>{
+
+  //constent creation===========
+  const addButton = document.getElementById('add-task-btn');
+  const taskInput = document.getElementById('task-input');
+  const taskList = document.getElementById('task-list');
+  
+
+  // addTask function============
+  function addTask() {
+    const taskText = taskInput.value.trim();
+
+
+    if (taskText=== "") {
+      alert("try enter a valid task!");
+    }
+    if (!taskText==="") {
+      const textBar = document.createElement('li');
+      textBar.textContent = taskText;
+
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = "Remove";
+
+      removeBtn.classList.add('remove-btn');
+      removeBtn.onclick = function () {
+        taskList.removeChild(taskBar);
+        removeTask(taskText);
+      }
+      taskInput.value = "";
+
+    }
+    addButton.addEventListener('click',() => addTask());
+    
+    taskInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
+    });
+
+    document.addEventListener('DOMContentLoaded', addTask);
+  }
+
+  
+  
+})
